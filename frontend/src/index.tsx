6b34742963e1931c28c5318ef1ae4a7b21222989
waqfs <43045469+waqfs@users.noreply.github.com>
2026-03-11 09:19:45 -0400

    link all routes together

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,21 +1,27 @@
 import { render } from 'preact';
 import { LocationProvider, Router, Route } from 'preact-iso';
 
-import { Header } from '@component/Header.jsx';
 import { Home } from '@route/Home/index.jsx';
+import { Login } from '@route/Login/index.jsx';
+import { Progress } from '@route/Progress/index.jsx';
+import { Exercise } from '@route/Exercise/index.jsx';
+import { Live } from '@route/Live/index.jsx';
+import { Settings } from '@route/Settings/index.jsx';
 import { NotFound } from '@route/_404.jsx';
 import './style.css';
 
 export function App() {
   return (
     <LocationProvider>
-      <Header />
-      <main>
-        <Router>
-          <Route path="/" component={Home} />
-          <Route default component={NotFound} />
-        </Router>
-      </main>
+      <Router>
+        <Route path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/progress" component={Progress} />
+        <Route path="/exercise" component={Exercise} />
+        <Route path="/live" component={Live} />
+        <Route path="/settings" component={Settings} />
+        <Route default component={NotFound} />
+      </Router>
     </LocationProvider>
   );
 }
