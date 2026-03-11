08a38562d0e147dc681ec1dd79f17836820edea8
waqfs <43045469+waqfs@users.noreply.github.com>
2026-03-11 00:02:57 -0400

    format everything

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,17 +7,17 @@ import { NotFound } from '@route/_404.jsx';
 import './style.css';
 
 export function App() {
-	return (
-		<LocationProvider>
-			<Header />
-			<main>
-				<Router>
-					<Route path="/" component={Home} />
-					<Route default component={NotFound} />
-				</Router>
-			</main>
-		</LocationProvider>
-	);
+  return (
+    <LocationProvider>
+      <Header />
+      <main>
+        <Router>
+          <Route path="/" component={Home} />
+          <Route default component={NotFound} />
+        </Router>
+      </main>
+    </LocationProvider>
+  );
 }
 
 render(<App />, document.getElementById('app'));
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [tsconfigPaths(), preact(), tailwindcss()],
+  plugins: [tsconfigPaths(), preact(), tailwindcss()],
 });
